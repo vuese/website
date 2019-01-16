3f8c7c1e24b7f23922f34e54d126fac39e4956ae
HcySunYang <HcySunYang@outlook.com>
2019-01-16 15:19:07 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,10 @@ git config user.email 'HcySunYang@outlook.com'
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
 git push -f git@github.com:vuese/website.git master:gh-pages
 
 cd -
+
+pwd
+
+rm -rf docs/.vuepress/dist
